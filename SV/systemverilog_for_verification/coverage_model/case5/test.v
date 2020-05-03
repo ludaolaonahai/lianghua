2f3017db63ee438d16e7d455ad2a99ab961ca1d8
ludaolaonanhai <394911731@qq.com>
2020-05-04 01:39:01 +0800

    m

---
+++ SV/systemverilog_for_verification/coverage_model/case5/test.v
@@ -0,0 +1,41 @@
+module top;
+class a;
+bit[7:0] bus;
+bit rst;
+
+covergroup g1;
+option.auto_bin_max = 4;
+
+value: 
+coverpoint bus iff (!rst) { 
+	ignore_bins dvalue[] =  { [0:23] }; 
+	illegal_bins cvalue   =  { 128 };
+}
+endgroup
+
+function new();
+g1 = new;
+endfunction
+
+endclass
+ 
+a obj1,obj2;
+initial begin
+int i=0;
+obj1 = new;
+repeat(1) begin
+	obj1.bus = i;
+	#1; 
+	obj1.g1.sample();
+end
+obj2 = new;
+obj2.rst = 1;
+repeat(1) begin
+	obj2.bus = i+128;
+	#1; 
+	obj2.g1.sample();
+end
+
+#100 $finish;
+end
+endmodule
